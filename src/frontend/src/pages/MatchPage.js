dfd9b6c292aab59394d3d2b133a4dea1d10c6fa8
JorgePereira09005 <jorge09005@gmail.com>
2021-05-23 17:05:08 +0100

    Started with styling on TeamPage component and its child components.

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -6,9 +6,35 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 //watch the goddamn video before complaining
 export const MatchPage = () => {
 
-  return (
-    <div className="MatchPage">
-        <h1>Match Page</h1>
-    </div>
-  );
+    //declare state variable and method to set that state
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+
+    useEffect(
+    
+        //executes when component is loaded
+        () => {
+          const fetchMatches = async () => {
+            const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+            const data = await response.json();
+            setMatches(data);
+          };
+    
+          fetchMatches();
+    
+        // empty array means "run only on first page load"
+        // reload page when property "teamName" changes
+        }, []
+      ); 
+
+    return (
+        <div className="MatchPage">
+            <h1>Match Page</h1>
+
+            {
+                matches.map(match => <MatchDetailCard teamName = {teamName} match = {match}/>)
+            }
+
+        </div>
+    );
 }
